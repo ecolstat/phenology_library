44f9697ccf2789743e8165d05debdb9cdb120e6d
ecolstat <darinschulte@gmail.com>
2020-03-26 22:38:47 -0600

    map color, added more info button, fixed y-axis dimensions on scatterplots

--- app4.py
+++ app4.py
@@ -56,6 +56,24 @@ lc_options = [
     {"label": str(NLCD_2011[lc_class]), "value": str(lc_class)} for lc_class in NLCD_2011
 ]
 
+infoModal = html.H1(
+    [
+        dbc.Button("Learn More", id="open-info"),
+        dbc.Modal(
+            [
+                dbc.ModalHeader("Project Description"),
+                dbc.ModalBody("JKFLDSJKFLDS:JFKDSL:JFKDSL:JFKDSL:JFKSDL:JFKDSL:"),
+                dbc.ModalFooter(
+                    dbc.Button(
+                        "Close", id="close-info", className="ml-auto"
+                    )
+                ),
+            ],
+            id="info-modal-centered",
+            centered=True,
+        ),
+    ]
+)
 controls = dbc.Row(
     [
         dbc.Col(
@@ -96,7 +114,7 @@ controls = dbc.Row(
 
 layout = dict(
     autosize=True,
-    height=600,
+    height=700,
     font=dict(color="#191A1A"),
     titlefont=dict(color="#191A1A", size='14'),
     margin=dict(
@@ -106,6 +124,7 @@ layout = dict(
         t=45
     ),
     hovermode="closest",
+    uirevision='never',
     # plot_bgcolor='#fffcfc',
     # paper_bgcolor='#fffcfc',
     legend=dict(font=dict(size=10), orientation='h'),
@@ -141,20 +160,49 @@ def gen_map(map_data):
                 'marker': {
                     'size': 6,
                     'opacity': 0.7,
-                    'color': map_data['ndvi']
+                    'color': map_data['ndvi'],
+                    'colorscale': [[0, 'white'], [1,'green']],
+                    'cmin': 0,
+                    'cmax': 10000
                 }
             }
         ],
         "layout": layout
     }
 
-
-
+tabPlots = html.Div(
+    [
+        dbc.Tabs(
+            [
+                dbc.Tab(label='Scatter', tab_id='scatter'),
+                dbc.Tab(label='Boxplot', tab_id='boxplot')
+            ],
+            id='tabs',
+        ),
+        html.Div(id='tab-content')
+    ]
+)
 app.layout = dbc.Container(
     [
-        html.H1('Phenology Library'),
-        html.P('Interactive mapping of NDVI signatures by land cover class.'),
-        # html.Hr(), #adds lines above and below the H1 and P
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.H1('Phenology Library'),
+                        html.P('Interactive mapping of NDVI signatures by land cover class.'),
+                    ],
+                    md=11,
+                ),
+                dbc.Col(
+                    [
+                        infoModal,
+                    ],
+
+                )
+            ],
+            justify='end'
+        ),
+
         dbc.Row(
             [
                 dbc.Col(
@@ -171,25 +219,9 @@ app.layout = dbc.Container(
         dbc.Row(
             [
                 dbc.Col(
-                        dbc.Tabs(
-                            [
-                                dbc.Tab(label='Scatter' ,tab_id='scatter'),
-                                dbc.Tab(label='BoxPlot', tab_id='boxplot'),
-                            ],
-                            id='tabs',
-                        ),
-                        html.Div(id='tab-content', className='p-4'),
+                        tabPlots,
                         md=6
                 ),
-
-
-                #     ],
-                # ),
-                    # dcc.Graph(
-                    #     id='all-lc-scatter'
-                    # ),
-                    # md=6),
-
                 dbc.Col(
                     dcc.Graph(
                         # TODO: Fix y axis range
@@ -197,12 +229,23 @@ app.layout = dbc.Container(
                     ),
                     md=6),
             ],
-            align='right',
         ),
     ],
     fluid=True,
 )
 
+# More info button modal
+@app.callback(
+    Output("info-modal-centered", "is_open"),
+    [Input("open-info", "n_clicks"), Input("close-info", "n_clicks")],
+    [State("info-modal-centered", "is_open")],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+# Map graph
 # TODO: see https://community.plotly.com/t/preserving-ui-state-like-zoom-in-dcc-graph-with-uirevision/15793
 #   about preventing auto resetting map with input (e.g., slider) change.
 @app.callback(
@@ -218,106 +261,36 @@ def map_selection(lc_ID, doy):
     dff = dff[dff['reference_date' ] == DOY2DATETIMEDICT[doy]]
     return gen_map(dff)
 
-# @app.callback(
-#     Output('tab-content', 'children'),
-#     [Input('tabs', 'active_tab'),
-#      Input('lc-class-dropdown', 'value')]
-# )
-# def render_tab_content(active_tab, value):
-#
-#     dff = df.copy()
-#     # Landcover class filter
-#     dff = dff[dff['LC_code'] == int(value)]
-#     df['doy'] = df['reference_date'].map(DATETIME2DOYDICT)
-#
-#     if active_tab is not None:
-#         if active_tab == 'scatter':
-#             return  dcc.Graph(
-#                         id='all-lc-scatter',
-#                         figure=px.scatter(dff, x='doy', y='ndvi', trendline='lowess'),
-#                         responsive= True,
-#                         style=dict(height='100%', width='100%')
-#                    ),
-#         elif active_tab == 'boxplot':
-#             return dcc.Graph(
-#                         id='all-lc-boxplot',
-#                         figure=px.violin(dff, x='doy', y='ndvi', box=True),
-#                         responsive= True,
-#                         style=dict(height='100%', width='100%')
-#                     ),
-
+# Tabs, scatterplot and boxplot
 @app.callback(
-    Output('all-lc-scatter', 'figure'),
-    [Input('lc-class-dropdown', 'value')]
+    Output('tab-content', 'children'),
+    [Input('tabs', 'active_tab'),
+     Input('lc-class-dropdown', 'value')]
 )
-def lc_scatter_update(value):
+def render_tab_content(active_tab, value):
+
     dff = df.copy()
     # Landcover class filter
     dff = dff[dff['LC_code'] == int(value)]
-    dff['doy'] = dff['reference_date'].map(DATETIME2DOYDICT)
+    dff['doy'] = df['reference_date'].map(DATETIME2DOYDICT)
 
-    return px.scatter(dff, x='doy', y='ndvi', trendline='lowess')
-    # return {
-    #     'data': [
-    #         dict(
-    #             type='scattergl',
-    #             x=list(dff['reference_date']),
-    #             y=list(dff['ndvi']),
-    #             # text=list(dff['PointID']),
-    #             mode='lines+markers',
-    #             opacity=0.7,
-    #             marker={
-    #                 'size': 5,
-    #                 'line': {'trendline': 'lowess'}
-    #             },
-    #
-    #         )
-    #     ],
-    #     'layout': dict(
-    #         xaxis={'title': 'Day Of Year (DOY)'},
-    #         yaxis={'title': 'NDVI'},
-    #         # margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-    #         legend={'x': 0, 'y': 1},
-    #         hovermode='closest'
-    #     )
-    # }
-    # return px.line(dff, x='doy', y='ndvi', line_shape='spline')
+    if active_tab is not None:
+        if active_tab == 'scatter':
+            return  dcc.Graph(
+                        id='all-lc-scatter',
+                        figure=px.scatter(dff, x='doy', y='ndvi', trendline='lowess', range_y= [0, 10000]),
+                        responsive= True,
+                        style=dict(height='100%', width='100%')
+                   ),
+        elif active_tab == 'boxplot':
+            return dcc.Graph(
+                        id='all-lc-boxplot',
+                        figure=px.violin(dff, x='doy', y='ndvi', box=True, range_y= [0, 10000]),
+                        responsive= True,
+                        style=dict(height='100%', width='100%')
+                    ),
 
-
-# def lc_scatter_update(value):
-#     dff = df.copy()
-#     # Landcover class filter
-#     dff = dff[dff['LC_code'] == int(value)]
-#     dff['doy'] = dff['reference_date'].map(DATETIME2DOYDICT)
-#     fig = go.Figure()
-#     fig.add_trace(go.Scatter(
-#         x = dff['doy'],
-#         y = dff['ndvi'],
-#         mode = 'markers',
-#         marker = dict(size=5),
-#         name='NDVI'
-#     ))
-#     fig.add_trace(go.Scatter(
-#         x = dff['doy'],
-#         y = signal.savgol_filter(dff['ndvi'],
-#                                  201,  # window size for filtering
-#                                  3), # order of fitted polynomial
-#         mode = 'lines',
-#         name = 'Savitzky-Golay'
-#     ))
-#     return fig
-
-@app.callback(
-    Output('all-lc-boxplot', 'figure'),
-    [Input('lc-class-dropdown', 'value')]
-)
-def lc_scatter_update(value):
-    dff = df.copy()
-    # Landcover class filter
-    dff = dff[dff['LC_code'] == int(value)]
-    dff['doy'] = dff['reference_date'].map(DATETIME2DOYDICT)
-    return px.violin(dff, x='doy', y='ndvi', box=True)
-
+# scatterplot responsive to map hover
 @app.callback(
     Output('ndvi-by-doy-scatter', 'figure'),
     [Input('lc-class-dropdown', 'value'),
@@ -342,24 +315,22 @@ def scatter_update(lc_ID, hoverData):
                 type='scattergl',
                 x=list(dff['reference_date']),
                 y=list(dff['ndvi']),
-                # text=list(dff['PointID']),
                 mode='lines+markers',
                 opacity=0.7,
+
                 marker={
                     'size': 9,
-                    # 'line': {'width': 0.5,
-                    #          'color': 'white'}
                 },
-                # name=41
-            )  # for i in df.LC_code.unique()
+            )
         ],
         'layout': dict(
             xaxis={'title': 'Day Of Year (DOY)'},
-            yaxis={'title': 'NDVI'},
+            yaxis={'title': 'NDVI', 'range': [0, 10000], 'fixedrange': True},
             # margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
             legend={'x': 0, 'y': 1},
             hovermode='closest'
         )
     }
+
 if __name__ == '__main__':
     app.run_server(debug=True)
